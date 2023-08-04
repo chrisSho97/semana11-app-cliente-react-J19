5ddf3ddaf348b82906545bafb53771a8a8364e82
chrisSho97 <134670235+chrisSho97@users.noreply.github.com>
2023-08-04 01:32:50 +0000

    modificando las tablas

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-	private final EmployeeRepository repository;
+	private final InstrumentoRepository repository;
 
 	@Autowired
-	public DatabaseLoader(EmployeeRepository repository) {
+	public DatabaseLoader(InstrumentoRepository repository) {
 		this.repository = repository;
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Instrumento("Guitarra", "Cuerda", "de madera"));
+		this.repository.save(new Instrumento("Ukelele", "Cuerda", "caja de resonancia pequeña"));
+		this.repository.save(new Instrumento("Melodica", "Viento", "Teclado de 4 cuerdas"));
 	}
 }
